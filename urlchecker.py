caa3445879b706732e0c96b7fec56b303a5a26e8
Michał Maciej Różański <michal.rozanski@gmail.com>
2013-06-11 15:26:46 +0200

    https support added

--- urlchecker.py
+++ urlchecker.py
@@ -3,7 +3,7 @@ from pyquery import PyQuery as pq
 def checkurl_title(c, e, channel):
     try:
         for word in e.arguments[0].split():
-            if 'http://' in word:
+            if word.startswith(('http://', 'https://')):
                 url = pq(url=word.strip())
                 title = url("title").text()
                 c.privmsg(channel, "[URL Title] " + title)
